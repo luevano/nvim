7bd97062c98418a9bdf31df0b84737da67bed38b
David Luevano Alvarado <david@luevano.xyz>
2023-06-04 05:10:35 -0600

    add undotree

--- init.lua
+++ init.lua
@@ -2,8 +2,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Install package manager
---    https://github.com/folke/lazy.nvim
+-- Install https://github.com/folke/lazy.nvim
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,7 @@
+return {
+  'mbbill/undotree',
+  -- lazy load on <leader>u
+  keys = {
+    { '<leader>u', '<CMD>UndotreeToggle<CR>', desc = 'Toggle [U]ndotree' },
+  },
+}
