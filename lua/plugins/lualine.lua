ad51c526e472ca958a2131270da9682bfa0a2492
David Luevano Alvarado <david@luevano.xyz>
2023-06-09 06:58:05 -0600

    add tabline to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,46 @@
 return {
   'nvim-lualine/lualine.nvim',
+  dependencies = {
+    { 'nvim-tree/nvim-web-devicons', opt = {} },
+  },
   opts = {
     options = {
+      theme = 'dracula',
       icons_enabled = true,
-      theme = 'dracula-nvim',
-      -- component_separators = '|',
-      -- section_separators = '',
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
     },
+    sections = {
+      lualine_a = {'mode'},
+      lualine_b = {'branch', 'diff', 'diagnostics'},
+      lualine_c = {'filename'},
+      lualine_x = {'encoding', 'fileformat', 'filetype'},
+      lualine_y = {'progress'},
+      lualine_z = {'location'},
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = {'filename'},
+      lualine_x = {'location'},
+      lualine_y = {},
+      lualine_z = {}
+    },
+    -- Very basic tabline support, for more
+    -- https://github.com/kdheepak/tabline.nvim
+    -- as it supports binding buffers to tabs so they can work as workspaces
+    tabline = {
+      lualine_a = {'buffers'},
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {'tabs'},
+    },
+    -- Window specific bar, appears below tabline
+    -- and inside window
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {'neo-tree'},
   },
 }
